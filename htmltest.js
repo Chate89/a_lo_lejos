81e26cd2b020a51b85bf28befae6e9e58a0f057a
Chate89 <gastonchatelet@gmail.com>
2017-06-07 12:30:29 -0300

    Add files via upload

---
+++ htmltest.js
@@ -0,0 +1,31 @@
+var track = [];
+var numMod = 6;
+
+for (var i = 0; i < numMod; i++) {
+  track[i] = document.createElement("audio");
+  track[i].src = "data/Module0" + (i+1) + "/00_1.mp3";
+  track[i].addEventListener('ended', function () {
+    track[i].pause();
+  }, false);
+}
+
+function play() {
+  for (var i = 0; i < track.length; i++) {
+    track[i].play();
+  }
+}
+
+function pause() {
+  for (var i = 0; i < track.length; i++) {
+    // track[i].currentTime = 0;
+    track[i].pause();
+    // track[i].currentTime = track[0].currentTime;
+  }
+}
+
+function stop() {
+  for (var i = 0; i < track.length; i++) {
+    track[i].currentTime = 0;
+    track[i].pause();
+  }
+}
